d59ec1cbf6898cfc199af9f55951d15b191289e5
AsrinManal <asrinmanal12005@gmail.com>
2026-01-20 22:50:13 +0530

    update payment.js

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { processPayment, sendStripeApiKey } = require("../controllers/paymentController");
+
+const {
+  processPayment,
+  sendStripeApiKey,
+} = require("../controllers/paymentcontroller"); // 👈 lowercase
 
 router.post("/payment/process", processPayment);
 router.get("/stripeapikey", sendStripeApiKey);
